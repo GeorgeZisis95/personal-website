48d617775d50c8ed509e9d6944b9d363c3130f8a
George Zisis <georgezisis@gmail.com>
2025-12-15 11:10:46 +0200

    Add google icons for navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Home from "./components/Home";
 import Navigation from "./components/Navigation";
 
 export default function App() {
   return (
     <>
       <Navigation />
+      <Home />
     </>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <h3>Hey, I'm George👋</h3>
+      <h1>
+        <span style={{ color: "var(--accent)" }}>Front</span>
+        <span>end Developer</span>
+      </h1>
+      <h3>
+        Based in Greece, I create clean, engaging websites designed for optimal
+        user experiences
+      </h3>
+    </div>
+  );
+}
