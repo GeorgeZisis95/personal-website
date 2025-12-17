95e467c01a16112518539b4a26561af8f6d99be6
George Zisis <georgezisis@gmail.com>
2025-12-17 14:37:23 +0200

    responsive navbar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,5 +1,9 @@
 import { useEffect, useState } from "react";
 import "./Navigation.css";
+import homeImg from "../assets/home.svg";
+import aboutImg from "../assets/about.svg";
+import skillsImg from "../assets/skills.svg";
+import projectsImg from "../assets/projects.svg";
 
 export default function Navigation() {
   const [scrolled, setScrolled] = useState(false);
@@ -25,39 +29,12 @@ export default function Navigation() {
         <span style={{ color: "var(--heading)" }}>George </span>
         <span style={{ color: "var(--accent)" }}>Zisis</span>
       </a>
+
       <div className="nav-buttons">
-        <Button
-          img={
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
-              <path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z" />
-            </svg>
-          }
-          text={"Home"}
-        />
-        <Button
-          img={
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
-              <path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Zm80-80h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z" />
-            </svg>
-          }
-          text={"About"}
-        />
-        <Button
-          img={
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
-              <path d="M754-81q-8 0-15-2.5T726-92L522-296q-6-6-8.5-13t-2.5-15q0-8 2.5-15t8.5-13l85-85q6-6 13-8.5t15-2.5q8 0 15 2.5t13 8.5l204 204q6 6 8.5 13t2.5 15q0 8-2.5 15t-8.5 13l-85 85q-6 6-13 8.5T754-81Zm0-95 29-29-147-147-29 29 147 147ZM205-80q-8 0-15.5-3T176-92l-84-84q-6-6-9-13.5T80-205q0-8 3-15t9-13l212-212h85l34-34-165-165h-57L80-765l113-113 121 121v57l165 165 116-116-43-43 56-56H495l-28-28 142-142 28 28v113l56-56 142 142q17 17 26 38.5t9 45.5q0 24-9 46t-26 39l-85-85-56 56-42-42-207 207v84L233-92q-6 6-13 9t-15 3Zm0-96 170-170v-29h-29L176-205l29 29Zm0 0-29-29 15 14 14 15Zm549 0 29-29-29 29Z" />
-            </svg>
-          }
-          text={"Skills"}
-        />
-        <Button
-          img={
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
-              <path d="M440-183v-274L200-596v274l240 139Zm80 0 240-139v-274L520-457v274Zm-40-343 237-137-237-137-237 137 237 137ZM160-252q-19-11-29.5-29T120-321v-318q0-22 10.5-40t29.5-29l280-161q19-11 40-11t40 11l280 161q19 11 29.5 29t10.5 40v318q0 22-10.5 40T800-252L520-91q-19 11-40 11t-40-11L160-252Zm320-228Z" />
-            </svg>
-          }
-          text={"Projects"}
-        />
+        <Button img={homeImg} text={"Home"} />
+        <Button img={aboutImg} text={"About"} />
+        <Button img={skillsImg} text={"Skills"} />
+        <Button img={projectsImg} text={"Projects"} />
       </div>
     </nav>
   );
@@ -67,7 +44,7 @@ function Button({ img, text }) {
   const id = `#${text.toLowerCase()}`;
   return (
     <a href={id} className="button">
-      {img}
+      <img src={img} alt={text} />
       <span style={{ color: "var(--heading)" }}>
         <h3>{text}</h3>
       </span>
