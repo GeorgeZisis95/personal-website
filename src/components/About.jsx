d40739021aa4f9f917105e45166a518b46014933
George Zisis <georgezisis@gmail.com>
2025-12-18 10:33:44 +0200

    Responsive about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,29 +10,31 @@ export default function About({ aboutRef }) {
           <span>Me</span>
         </div>
       </h2>
-      <p style={{ color: "var(--text)" }}>
-        I’m a self-taught web developer with a strong focus on building clean,
-        responsive, and user-friendly interfaces. I enjoy turning ideas into
-        practical, functional digital experiences.
-        <br />
-        <br />I have hands-on experience with HTML, CSS, and JavaScript and I
-        work mainly with modern tools and frameworks like React. I’m constantly
-        learning and improving my skills through real-world projects and
-        consistent practice.
-        <br />
-        <br />
-        I come from a practical background, with experience in customer-facing
-        environments, which helps me understand users, usability, and clear
-        communication. I value clean code, attention to detail, and creating
-        interfaces that feel intuitive and fast.
-        <br />
-        <br />I enjoy reading literature and exploring history, as well as
-        learning about cultures and different time periods. I like being with
-        friends, playing card games, and discovering new cafés, restaurants, and
-        places to relax. These activities keep me curious, connected, and
-        inspired.
-      </p>
-      <img src={hipsterImg} alt="knightImg" />
+      <div className="about-container">
+        <img src={hipsterImg} alt="hipsterImg" />
+        <p style={{ color: "var(--text)" }}>
+          I’m a self-taught web developer with a strong focus on building clean,
+          responsive, and user-friendly interfaces. I enjoy turning ideas into
+          practical, functional digital experiences.
+          <br />
+          <br />I have hands-on experience with HTML, CSS, and JavaScript and I
+          work mainly with modern tools and frameworks like React. I’m
+          constantly learning and improving my skills through real-world
+          projects and consistent practice.
+          <br />
+          <br />
+          I come from a practical background, with experience in customer-facing
+          environments, which helps me understand users, usability, and clear
+          communication. I value clean code, attention to detail, and creating
+          interfaces that feel intuitive and fast.
+          <br />
+          <br />I enjoy reading literature and exploring history, as well as
+          learning about cultures and different time periods. I like being with
+          friends, playing card games, and discovering new cafés, restaurants,
+          and places to relax. These activities keep me curious, connected, and
+          inspired.
+        </p>
+      </div>
     </section>
   );
 }
