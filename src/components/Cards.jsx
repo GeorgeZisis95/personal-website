6da0490ce60cf28c934d89b52280e91a321d324d
George Zisis <georgezisis@gmail.com>
2025-12-19 11:52:28 +0200

    Add 2 projects, fix ratior of card

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,12 +1,12 @@
 import javascriptImg from "../assets/skills/javascript.svg";
 import cssImg from "../assets/skills/css.svg";
-// import htmlImg from "../assets/skills/html.svg";
+import htmlImg from "../assets/skills/html.svg";
 import reactImg from "../assets/skills/react.svg";
 import figmaImg from "../assets/skills/figma.svg";
 import viteImg from "../assets/skills/vite.svg";
 // import pythonImg from "../assets/skills/python.svg";
 import gitImg from "../assets/skills/git.svg";
-// import webpackImg from "../assets/skills/webpack.svg";
+import webpackImg from "../assets/skills/webpack.svg";
 import vercelImg from "../assets/skills/vercel.svg";
 
 export const cards = [
@@ -29,138 +29,18 @@ export const cards = [
   },
   {
     id: 2,
-    title: "Landing Page",
-    desc: "Real world project for the monastery of Saint Kosmas - Thermon",
-    images: [
-      reactImg,
-      javascriptImg,
-      gitImg,
-      cssImg,
-      viteImg,
-      figmaImg,
-      vercelImg,
-    ],
-    websiteLink: "https://monastery-saint-kosmas-thermo.vercel.app/",
-    githubLink:
-      "https://github.com/GeorgeZisis95/monastery-saint-kosmas-thermo",
+    title: "Calculator",
+    desc: "A simple yet sleek calculator built with vanilla JavaScript",
+    images: [javascriptImg, htmlImg, cssImg, gitImg, webpackImg, vercelImg],
+    websiteLink: "https://zisis-calculator.vercel.app/",
+    githubLink: "https://github.com/GeorgeZisis95/calculator-js",
   },
   {
     id: 3,
-    title: "Landing Page",
-    desc: "Real world project for the monastery of Saint Kosmas Thermon",
-    images: [
-      reactImg,
-      javascriptImg,
-      gitImg,
-      cssImg,
-      viteImg,
-      figmaImg,
-      vercelImg,
-    ],
-    websiteLink: "https://monastery-saint-kosmas-thermo.vercel.app/",
-    githubLink:
-      "https://github.com/GeorgeZisis95/monastery-saint-kosmas-thermo",
-  },
-  {
-    id: 4,
-    title: "Landing Page",
-    desc: "Real world project for the monastery of Saint Kosmas Thermon",
-    images: [
-      reactImg,
-      javascriptImg,
-      gitImg,
-      cssImg,
-      viteImg,
-      figmaImg,
-      vercelImg,
-    ],
-    websiteLink: "https://monastery-saint-kosmas-thermo.vercel.app/",
-    githubLink:
-      "https://github.com/GeorgeZisis95/monastery-saint-kosmas-thermo",
-  },
-  {
-    id: 5,
-    title: "Landing Page",
-    desc: "Real world project for the monastery of Saint Kosmas Thermon",
-    images: [
-      reactImg,
-      javascriptImg,
-      gitImg,
-      cssImg,
-      viteImg,
-      figmaImg,
-      vercelImg,
-    ],
-    websiteLink: "https://monastery-saint-kosmas-thermo.vercel.app/",
-    githubLink:
-      "https://github.com/GeorgeZisis95/monastery-saint-kosmas-thermo",
-  },
-  {
-    id: 6,
-    title: "Landing Page",
-    desc: "Real world project for the monastery of Saint Kosmas Thermon",
-    images: [
-      reactImg,
-      javascriptImg,
-      gitImg,
-      cssImg,
-      viteImg,
-      figmaImg,
-      vercelImg,
-    ],
-    websiteLink: "https://monastery-saint-kosmas-thermo.vercel.app/",
-    githubLink:
-      "https://github.com/GeorgeZisis95/monastery-saint-kosmas-thermo",
-  },
-  {
-    id: 7,
-    title: "Landing Page",
-    desc: "Real world project for the monastery of Saint Kosmas Thermon",
-    images: [
-      reactImg,
-      javascriptImg,
-      gitImg,
-      cssImg,
-      viteImg,
-      figmaImg,
-      vercelImg,
-    ],
-    websiteLink: "https://monastery-saint-kosmas-thermo.vercel.app/",
-    githubLink:
-      "https://github.com/GeorgeZisis95/monastery-saint-kosmas-thermo",
-  },
-  {
-    id: 8,
-    title: "Landing Page",
-    desc: "Real world project for the monastery of Saint Kosmas Thermon",
-    images: [
-      reactImg,
-      javascriptImg,
-      gitImg,
-      cssImg,
-      viteImg,
-      figmaImg,
-      vercelImg,
-    ],
-    websiteLink: "https://monastery-saint-kosmas-thermo.vercel.app/",
-    githubLink:
-      "https://github.com/GeorgeZisis95/monastery-saint-kosmas-thermo",
-  },
-  {
-    id: 9,
-    title: "Landing Page",
-    desc: "Real world project for the monastery of Saint Kosmas Thermon",
-    images: [
-      reactImg,
-      javascriptImg,
-      gitImg,
-      cssImg,
-      viteImg,
-      figmaImg,
-      vercelImg,
-    ],
-    websiteLink: "https://monastery-saint-kosmas-thermo.vercel.app/",
-    githubLink:
-      "https://github.com/GeorgeZisis95/monastery-saint-kosmas-thermo",
+    title: "Tictactoe",
+    desc: "Modern & Responsive TicTacToe",
+    images: [javascriptImg, htmlImg, cssImg, gitImg, vercelImg],
+    websiteLink: "https://zisis-tictactoe.vercel.app/",
+    githubLink: "https://github.com/GeorgeZisis95/tic-tac-toe-js",
   },
 ];
